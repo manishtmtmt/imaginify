c1eda0d95f14d9e918eccd3fdcf8fba8ae15eaf0
manishtmtmt <manisht.mt.mt@gmail.com>
2024-02-20 17:03:17 +0530

    made conn to mongodb and created models

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -4,7 +4,6 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 
 import { navLinks } from "@/constants";
